9b95f8167a6e4d3b3be50c25593bdd6381fd8a13
UnDeadCharger <rexfury121@gmail.com>
2022-12-06 20:37:23 +0700

    Connect App to MySql Database

--- server/index.js
+++ server/index.js
@@ -1,9 +1,22 @@
 const express = require('express');
 const app = express();
+let mysql = require('mysql2');
+
+const db = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    password: 'ShadowWarrior2',   
+    database: 'cruddatabase',
+});
 
 app.get('/', (req, res) =>{
-    res.send("Hello wod");
-})
+    const sqlInsert = "INSERT INTO movie_review (id, movieName, movieReview) VALUES ('3', 'Inception', 'Good Movie');"
+   
+    db.query(sqlInsert, (err, result)=>{
+        res.send("Hello wod");
+    });
+   
+});
 
 app.listen(3001, () => {
     console.log("running on port 3001");
