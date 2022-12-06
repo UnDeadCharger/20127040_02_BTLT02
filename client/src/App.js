3b2f91c2dd63e5c567f11273e4f00552116a2379
UnDeadCharger <rexfury121@gmail.com>
2022-12-06 19:14:56 +0700

    commit msg

---
+++ client/src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      hello
+      <h1>CRUD APPLICATION</h1>
+      <div className="form">
+        <label> Movie Name that you hate</label>
+        <input type="text" name="movieName"></input>
+        <label> Review</label>
+        <input type="text" name="movieName"></input>
+      
+      <button>Submit</button>      
+     </div>
+    </div>
+  );
+}
+
+export default App;
